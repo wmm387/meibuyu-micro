85801d3156bf91952acd311c10c368ce76fe6dff
王源 <wangyuanwmm@163.com>
2020-03-16 09:36:31 +0800

    添加仓库服务接口文件

---
+++ src/Service/Interfaces/StoreServiceInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 王源
+ * Date: 2020/3/16
+ * Time: 15:07
+ */
+
+namespace Meibuyu\Micro\Service\Interfaces;
+
+interface StoreServiceInterface
+{
+    //
+}
