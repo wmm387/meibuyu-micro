b5bd64f2bda90c87a8e6c678581b1a8bfac04298
王源 <wangyuanwmm@163.com>
2020-05-15 17:27:55 +0800

    修改子sku rpc服务接口

--- src/Service/Interfaces/ProductChildServiceInterface.php
+++ src/Service/Interfaces/ProductChildServiceInterface.php
@@ -14,17 +14,17 @@ interface ProductChildServiceInterface
     /**
      * 获取单个数据
      * @param int $id 子SKU id
-     * @param array $columns
-     * @param array $relations 子SKU的关联关系
-     * @return array
+     * @param array $columns 子SKU表的字段，默认显示全部
+     * @param array $relations 子SKU的关联关系,可传入['color', 'size', 'brand', 'category', 'product_name', 'images', 'cost']
+     * @return array|null
      */
-    public function get($id, array $columns = ['*'], array $relations = []): array;
+    public function get($id, array $columns = ['*'], array $relations = []);
 
     /**
      * 通过id列表获取产品数组
      * @param array $idList 子SKUid的列表
      * @param array $columns 子SKU表的字段，默认显示全部
-     * @param array $relations 子SKU的关联关系
+     * @param array $relations 子SKU的关联关系,可传入['color', 'size', 'brand', 'category', 'product_name', 'images', 'cost']
      * @return array
      */
     public function getByIdList(array $idList, array $columns = ['*'], array $relations = []): array;
